7b88196269887e0e47bdf21d9f24510c7fe15afc
stillawake17 <118810624+stillawake17@users.noreply.github.com>
2023-01-31 13:36:57 +0000

    I've added the time, added the scripts

---
+++ starter/script.js
@@ -0,0 +1,15 @@
+var timeDisplayEl = $('#time-display');
+
+// handle displaying the time
+function displayTime() {
+    var rightNow = moment().format('DD MMM YYYY [at] hh:mm:ss a');
+    timeDisplayEl.text(rightNow);
+  }
+
+  
+
+
+
+
+
+  setInterval(displayTime, 1000);
