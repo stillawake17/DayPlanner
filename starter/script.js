6d3157cf089708f5f241c1347a5a118a22433d3a
Joanna Booth <118810624+stillawake17@users.noreply.github.com>
2023-02-01 21:53:34 +0000

    I'm creating the table dynamically instead and adding divs, classes and a container

--- starter/script.js
+++ starter/script.js
@@ -1,4 +1,6 @@
 var timeDisplayEl = $('#time-display');
+var currentTime = moment();
+alert(currentTime);
 
 // handle displaying the time
 function displayTime() {
@@ -9,12 +11,31 @@ function displayTime() {
   
 
 // handle printing the schedule and its data to the page
-
-function printPlannerData() {
-  var projectRowEl = $('<tr>');
-}
-
-
+const Container= $('<div>').addClass("container");
+const text1= $('<textarea>').addClass("description");
+const input1 = $('<input>').addClass("input");
+const text2= $('<textarea>').addClass("description");
+const input2 = $('<input>').addClass("input");
+const text3= $('<textarea>').addClass("description");
+const input3 = $('<input>').addClass("input");
+const text4= $('<textarea>').addClass("description");
+const input4 = $('<input>').addClass("input");
+const text5= $('<textarea>').addClass("description");
+const input5 = $('<input>').addClass("input");
+const text6= $('<textarea>').addClass("description");
+const input6 = $('<input>').addClass("input");
+const text7= $('<textarea>').addClass("description");
+const input7 = $('<input>').addClass("input");
+const text8= $('<textarea>').addClass("description");
+const input8 = $('<input>').addClass("input");
+const text9= $('<textarea>').addClass("description");
+const input9 = $('<input>').addClass("input");
+const text10= $('<textarea>').addClass("description");
+const input10 = $('<input>').addClass("input");
+const text11= $('<textarea>').addClass("description");
+const input11 = $('<input>').addClass("input");
+const text12= $('<textarea>').addClass("description");
+const input12 = $('<input>').addClass("input");
 
 
   setInterval(displayTime, 1000);
